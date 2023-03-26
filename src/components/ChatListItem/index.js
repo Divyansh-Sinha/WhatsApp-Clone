5f4260ceb837ddf04adc152658738185bcc056d1
Divyansh Sinha <61075271+Divyansh-Sinha@users.noreply.github.com>
2023-03-26 14:36:55 +0530

    Using Props

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,13 +1,12 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 
-const ChatListItem = () => {
+const ChatListItem = ({chat}) => {
+    
     return (
         <View style={styles.container}>
             {/* User Avatar */}
             <Image
-                source={{
-                    uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
-                }}
+                source={{ uri: chat.user.image  }}
                 style={styles.image}
             />
 
@@ -15,11 +14,15 @@ const ChatListItem = () => {
             <View style={styles.content}>
                 {/* Row */}
                 <View style={styles.row}>
-                    <Text numberOfLines={1} style={styles.name}>Lukas</Text>
-                    <Text style={styles.subTitle}>07:30</Text>
+                    <Text numberOfLines={1} style={styles.name}>
+                        {chat.user.name}
+                    </Text>
+                    <Text style={styles.subTitle}>{chat.lastMessage.createdAt}</Text>
                 </View>
 
-                <Text numberOfLines={2} style={styles.subTitle}>Oke</Text>
+                <Text numberOfLines={2} style={styles.subTitle}>
+                    {chat.lastMessage.text}
+                </Text>
             </View>
         </View>
     );
